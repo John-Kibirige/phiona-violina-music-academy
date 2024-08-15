1a8b5d49a41476164779f2d07976ef058498eaa4
kibirige <kibirigejohn005@gmail.com>
2024-08-15 04:40:24 +0300

    before adding swiper

---
+++ src/components/home/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <section className="hero-container w-screen h-screen border-2 border-primary">
+      <h1 className=" font-league-spartan text-center text-primary font-semibold text-5xl leading-10 mt-10">
+        Transforming Dreams Into Melodies
+      </h1>
+    </section>
+  );
+};
+
+export default Hero;
