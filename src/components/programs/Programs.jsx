95265f2aa16cacaf687948c7724bc2fd5ced60e6
Mileslegend <mileslegend7@gmail.com>
2024-10-24 17:06:45 +0000

    Am implementing soon

--- src/components/programs/Programs.jsx
+++ src/components/programs/Programs.jsx
@@ -4,6 +4,8 @@ const Programs = () => {
   return (
     <div>
       programs page
+      {/* i have been developing some css..and trying something here and there just the
+       challenge they were not flowing well with react am working on it */}
     </div>
   )
 }
